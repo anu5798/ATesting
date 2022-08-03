609a584b521b809826434110ceb0d03361aaad9e
ANUSHA <ANUSHA@DESKTOP-16NIVLQ>
2022-08-03 11:03:03 +0500

    By Anu

--- Testing_Git/src/com/cjc/Demo.java
+++ Testing_Git/src/com/cjc/Demo.java
@@ -12,6 +12,12 @@ public class Demo
 		System.out.println("Hello---m2");
 	}
 	
+	public void m3()
+	{
+		System.out.println("Hello---m3");
+	}
+	
+	
 	
 	public static void main(String[] args) {
 		
